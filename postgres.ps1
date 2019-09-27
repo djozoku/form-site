eedd6a13cb20d42935f577c0895e585b9ad2d5b0
Ozoku <dragon.djozoku@gmail.com>
2019-09-27 12:49:45 +0300

    chore(dev): make sleep longer for container startup wait

--- postgres.ps1
+++ postgres.ps1
@@ -1,5 +1,5 @@
 docker run --name form-postgres -e POSTGRES_PASSWORD=ozokudev -p 5432:5432 -d postgres
-Start-Sleep -s 1
+Start-Sleep -s 10
 docker exec -it form-postgres runuser -l postgres -c 'createdb form-db'
 Start-Sleep -s 1
 pause
