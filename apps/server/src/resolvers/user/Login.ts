41ae28e467ad430d6a15e200264a43fbcce7c861
Ozoku <dragon.djozoku@gmail.com>
2019-10-03 14:48:40 +0300

    fix(login): add context to login mutation

--- apps/server/src/resolvers/user/Login.ts
+++ apps/server/src/resolvers/user/Login.ts
@@ -1,14 +1,18 @@
-import { Resolver, Arg, Mutation } from 'type-graphql';
+import { Resolver, Arg, Mutation, Ctx } from 'type-graphql';
 
 import User from '../../entity/User';
 import { comparePassword, createAccessToken, createRefreshToken } from '../../utils/auth';
 import LoginInput from './inputs/LoginInput';
 import LoginResponse from './responses/LoginResponse';
+import { MyContext } from '../../types/MyContext';
 
 @Resolver()
 export default class LoginResolver {
   @Mutation(() => LoginResponse, { nullable: true })
-  async login(@Arg('user') { username, password }: LoginInput): Promise<LoginResponse> {
+  async login(
+    @Arg('user') { username, password }: LoginInput,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
     const user = await User.findOne({ where: { username } });
 
     if (!user) {
