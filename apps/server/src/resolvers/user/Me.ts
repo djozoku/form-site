73a0d33aebfec7de5f039acc3bb43f98e41bbaac
Ozoku <dragon.djozoku@gmail.com>
2019-10-04 14:20:12 +0300

    feat(user-resolver): add me resolver

--- apps/server/src/resolvers/user/Me.ts
+++ apps/server/src/resolvers/user/Me.ts
@@ -1,12 +1,12 @@
-import { Query, Resolver, Ctx } from 'type-graphql';
-// import User from '../../entity/User';
+import { Query, Resolver, Ctx, Authorized } from 'type-graphql';
+import User from '../../entity/User';
 import { MyContext } from '../../types/MyContext';
 
 @Resolver()
 export default class MeResolver {
-  @Query(() => String, { nullable: true })
-  async me(@Ctx() { req }: MyContext) {
-    // return User.findOne(req.session.userid);
-    return 'Hello';
+  @Authorized()
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { userId }: MyContext) {
+    return User.findOne(userId!);
   }
 }
