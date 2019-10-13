f92c6d171ac219d15bfe5235e76cc20146ec117a
Ozoku <dragon.djozoku@gmail.com>
2019-10-13 20:22:40 +0300

    fix(build): code now builds with resolvers fixed

--- apps/server/src/resolvers/user/index.ts
+++ apps/server/src/resolvers/user/index.ts
@@ -1,8 +1,8 @@
-import RegisterResolver from './Register';
-import LoginResolver from './Login';
-import MeResolver from './Me';
-import ConfirmEmailResolver from './ConfirmEmail';
-import LogoutResolver from './Logout';
+import RegisterResolver from './register/Register.Resolver';
+import LoginResolver from './login/Login.Resolver';
+import MeResolver from './me/Me.Resolver';
+import ConfirmEmailResolver from './confirmEmail/ConfirmEmail.Resolver';
+import LogoutResolver from './logout/Logout.Resolver';
 
 const UserResolvers = [
   RegisterResolver,
