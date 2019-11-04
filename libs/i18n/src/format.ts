c0e5e821cd47937cfd685f84a223be6441534a8a
Ozoku <dragon.djozoku@gmail.com>
2019-11-04 11:48:50 +0200

    feat(i18n): add i18n library

---
+++ libs/i18n/src/format.ts
@@ -0,0 +1,45 @@
+import { FormatFunction } from 'i18next';
+
+const ownFormat = (value: string, lng: string) => {
+  switch (lng) {
+    case 'fi':
+      switch (value) {
+        case 'Salasana':
+          return 'Salasanan';
+        case 'Käyttäjänimi':
+          return 'Käyttäjänimen';
+        default:
+          return value;
+      }
+    default:
+      return value;
+  }
+};
+
+const inFormat = (value: string, lng: string) => {
+  switch (lng) {
+    case 'fi':
+      switch (value) {
+        case 'Salasana':
+          return 'Salasanassa';
+        default:
+          return value;
+      }
+    default:
+      return value;
+  }
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const formatFunc: FormatFunction = (value: string, format, lng) => {
+  switch (format) {
+    case 'own':
+      return ownFormat(value, lng!);
+    case 'in':
+      return inFormat(value, lng!);
+    case 'lower':
+      return value.toLowerCase();
+    default:
+      return value;
+  }
+};
