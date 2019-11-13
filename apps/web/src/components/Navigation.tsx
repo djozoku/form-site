cb21dcd7b46bd10e01499a71a746ce91c5b249e4
Ozoku <dragon.djozoku@gmail.com>
2019-11-13 13:19:25 +0200

    feat(web): add dropdown to user button

--- apps/web/src/components/Navigation.tsx
+++ apps/web/src/components/Navigation.tsx
@@ -1,14 +1,48 @@
 import React, { useState } from 'react';
-import { Menu, Icon, Avatar, Row, Col, Typography } from 'antd';
+import { Menu, Icon, Avatar, Row, Col, Typography, Dropdown } from 'antd';
+import { useWindowSize } from '@form/hooks';
+
+import './Navigation.css';
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3" disabled>
+      3rd menu item（disabled）
+    </Menu.Item>
+  </Menu>
+);
 
 const Navigation: React.FC = () => {
+  const { width } = useWindowSize();
+  const [userClassNames, setUserClassNames] = useState('navigation');
   const [current, setCurrent] = useState('home');
+
   const handleClick = (e: { key: string }) => {
     setCurrent(e.key);
   };
+
+  const onHover = () => {
+    if (userClassNames === 'navigation') {
+      setUserClassNames('navigation navigation-hover');
+    } else {
+      setUserClassNames('navigation');
+    }
+  };
+
   return (
     <Row>
-      <Col span={16}>
+      <Col span={14}>
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
           <Menu.Item key="home">
             <Icon type="home" />
@@ -20,13 +54,25 @@ const Navigation: React.FC = () => {
           </Menu.Item>
         </Menu>
       </Col>
-      <Col span={2} push={6}>
-        <div style={{ position: 'absolute', right: '10px', top: '-10px', height: '48px' }}>
-          <Typography.Text style={{ position: 'relative', top: '2px', right: '10px' }}>
-            Ozoku
-          </Typography.Text>
-          <Avatar icon="user" />
-        </div>
+      <Col span={4} push={6}>
+        <Dropdown overlay={menu}>
+          <div
+            style={{
+              position: 'absolute',
+              right: 0,
+              lineHeight: '46px',
+              paddingLeft: '20px',
+              paddingRight: '6px',
+              top: '1px'
+            }}
+            className={userClassNames}
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            <Typography.Text style={{ paddingRight: '20px' }}>Ozoku</Typography.Text>
+            <Avatar icon="user" style={{ position: 'relative', right: '8px', top: '-2px' }} />
+          </div>
+        </Dropdown>
       </Col>
     </Row>
   );
