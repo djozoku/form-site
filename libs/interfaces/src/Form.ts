1ab9805db16b51ebdc80079f9f56c192f8164ba6
Ozoku <dragon.djozoku@gmail.com>
2019-11-18 13:55:10 +0200

    feat(interfaces): type form data

--- libs/interfaces/src/Form.ts
+++ libs/interfaces/src/Form.ts
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-cycle */
 import { Group } from './Group';
+import { FormField, FormDataDisplay } from './FormData';
 
 export interface Form {
   id: number;
   name: string;
+  displayName: string;
   owner?: Group;
+  fields: FormField[];
+  dataDisplay: FormDataDisplay[];
 }
---
+++ libs/interfaces/src/FormData.ts
@@ -0,0 +1,56 @@
+export type FormFieldDatabaseType = 'text' | 'int' | 'real' | 'bool';
+export type FormFieldInputType = 'checkbox' | 'radio' | 'select' | 'text' | 'number';
+
+export interface FormField {
+  name: string;
+  displayName: string;
+  databaseType: FormFieldDatabaseType;
+  inputType: FormFieldInputType;
+  options?: string[];
+}
+
+export interface FormDisplay<T> {
+  type: T;
+}
+
+export interface CalculationDisplay<Left extends FormDisplay<any>, Right extends FormDisplay<any>>
+  extends FormDisplay<'calculation'> {
+  operation: '+' | '-' | '*' | '/' | '%';
+  left: Left;
+  right: Right;
+}
+
+export type BooleanConditionTypes = '&' | '|';
+
+export type ValueConditionTypes = '=' | '<' | '>' | '<=' | '>=' | '!=';
+
+export interface BaseCondition<T> {
+  conditionType: T;
+}
+
+export type BooleanCondition = BaseCondition<BooleanConditionTypes>;
+
+export interface ValueCondition extends BaseCondition<ValueConditionTypes> {
+  fieldName: string;
+  data: string | number | boolean;
+}
+
+export type Condition = ValueCondition | BooleanCondition;
+
+export interface CountDisplay extends FormDisplay<'count'> {
+  conditions: Condition & ValueCondition[];
+}
+
+export type FormDisplays = CalculationDisplay<FormDisplays, FormDisplays> | CountDisplay;
+
+export interface FormDataDisplay {
+  name: string;
+  displayName: string;
+  display: FormDisplays;
+}
+
+export interface FormData {
+  displayName?: string;
+  fields: FormField[];
+  dataDisplay: FormDataDisplay[];
+}
