a1063e3dfd8226d2734a4c7da3b8a1250a41387e
Ozoku <dragon.djozoku@gmail.com>
2019-11-18 20:29:42 +0200

    refactor(server): make user existance testing file cleaner

--- apps/server/src/modules/user/utils/userExists.ts
+++ apps/server/src/modules/user/utils/userExists.ts
@@ -2,11 +2,10 @@ import User from '../User.Entity';
 
 export const userExists = async (property: string, value: string) => {
   if (property === 'username') {
-    console.log('username');
-    return User.findOne({ where: { username: value } }).then((val) => typeof val !== 'undefined');
+    return User.findOne({ where: { username: value } }).then((val) => !!val);
   }
   if (property === 'email') {
-    return User.findOne({ where: { email: value } }).then((val) => typeof val !== 'undefined');
+    return User.findOne({ where: { email: value } }).then((val) => !!val);
   }
   return false;
 };
