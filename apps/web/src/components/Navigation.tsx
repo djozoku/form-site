a05948565eb06c52e0739f3087df4e2e74cb1239
Ozoku <dragon.djozoku@gmail.com>
2019-11-18 13:58:59 +0200

    feat(web): actual menu item

--- apps/web/src/components/Navigation.tsx
+++ apps/web/src/components/Navigation.tsx
@@ -7,18 +7,7 @@ import './Navigation.css';
 const menu = (
   <Menu>
     <Menu.Item key="0">
-      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-        1st menu item
-      </a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-        2nd menu item
-      </a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3" disabled>
-      3rd menu item（disabled）
+      <Link to="/account/login">Login</Link>
     </Menu.Item>
   </Menu>
 );
