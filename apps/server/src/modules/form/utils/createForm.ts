9956dc35cb8386ab2de3833d941cb3af90da4be7
Ozoku <dragon.djozoku@gmail.com>
2019-11-26 01:18:11 +0200

    fix(server): create form now correctly creates the form

--- apps/server/src/modules/form/utils/createForm.ts
+++ apps/server/src/modules/form/utils/createForm.ts
@@ -11,16 +11,15 @@ export const createForm = async (
   formName: string,
   formData: FormData
 ): Promise<boolean> => {
-  const form = new Form();
-  form.name = formName;
-  form.displayName = formData.displayName || formName;
-  form.owner = group;
-  form.fields = formData.fields;
-  form.dataDisplay = formData.dataDisplay;
+  const form = Form.create({
+    name: formName,
+    displayName: formData.displayName || formName,
+    owner: group,
+    fields: formData.fields,
+    dataDisplay: formData.dataDisplay
+  });
   const columns = formData.fields!.map((f) => `, ${f.name} ${f.databaseType}`).join('');
-  await getConnection().query(
-    `CREATE TABLE ${formName}_Form (ID int NOT NULL PRIMARY KEY AUTO_INCREMENT${columns});`
-  );
+  await getConnection().query(`CREATE TABLE ${formName}_Form (ID SERIAL PRIMARY KEY ${columns});`);
   await getManager().save(form);
   return true;
 };
