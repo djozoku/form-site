30d5d727592373684e06a795f661dcef1c70b73f
Ozoku <dragon.djozoku@gmail.com>
2019-12-03 13:07:20 +0200

    fix(server): correct field name parsing & insert query

--- apps/server/src/modules/form/utils/addFormDocument.ts
+++ apps/server/src/modules/form/utils/addFormDocument.ts
@@ -4,12 +4,18 @@ import Form from '../Form.Entity';
 
 export const addFormDocument = async (form: Form, document: string) => {
   const parsedDocument = JSON.parse(document);
-  let allOk = false;
-  Object.keys(parsedDocument).forEach((key) => {
-    allOk = allOk ? !!form.fields.find((field) => field.name === key) : false;
-  });
+  const correctField = (key: string) => form.fields.find((field) => field.displayName === key);
+  const allOk = Object.keys(parsedDocument).reduce<boolean>((acc, key, i) => {
+    if (i === 0) {
+      return !!correctField(key);
+    }
+    return acc && !!correctField(key);
+  }, false);
   if (!allOk) return false;
-  const sql = SQLBricks.insert(`${form.name}_Form`, parsedDocument).toParams();
+  const correctedDocument = Object.fromEntries(
+    Object.entries(parsedDocument).map(([k, v]) => [k.toLowerCase(), v])
+  );
+  const sql = SQLBricks.insert(`${form.name.toLowerCase()}_form`, correctedDocument).toParams();
   await getConnection().query(sql.text, sql.values);
   return true;
 };
